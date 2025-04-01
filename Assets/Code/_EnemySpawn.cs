8e22f3c31deee5990df1bef2e284ea44816a34c3
verylowpower <doudqwer@gmail.com>
2025-04-01 15:21:35 +0700

    add random spawn positon

--- Assets/Code/_EnemySpawn.cs
+++ Assets/Code/_EnemySpawn.cs
@@ -7,6 +7,8 @@ public class _EnemySpawn : MonoBehaviour
     [SerializeField] private float _spawnTime = 0f;
     [SerializeField] private float _minSpawnTime = 0f;
     [SerializeField] private float _maxSpawnTime = 0f;
+    [SerializeField] private int _spawnCount = 0;
+    public BoxCollider2D colliderArea;
 
     void Awake()
     {
@@ -16,17 +18,28 @@ public class _EnemySpawn : MonoBehaviour
 
     void Update()
     {
+        var randomSpawnPos = GetRandomPosition();
         _spawnTime -= Time.deltaTime;
         if (_spawnTime <= 0f)
         {
-            Instantiate(_enemyPrefap, transform.position, Quaternion.identity);
+            Instantiate(_enemyPrefap, randomSpawnPos, Quaternion.identity);
             SetSpawnTime();
         }
-    }
-
+    }   
+    
     private void SetSpawnTime()
     {
-        _spawnTime = Random.Range(_minSpawnTime, _maxSpawnTime); //comment
+        _spawnTime = Random.Range(_minSpawnTime, _maxSpawnTime);
     }
 
+    private Vector2 GetRandomPosition()
+    {
+        var basePos = colliderArea.transform.position;
+        var size = colliderArea.size;
+        var posX = basePos.x + Random.Range(-size.x, size.x);
+        var posY = basePos.x + Random.Range(-size.y, size.y);
+
+        var spawnPos = new Vector2(posX, posY);
+        return spawnPos;
+    }
 }
